6da071542ed73d8d963c92b33b52e39dfdb61fa4
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-23 21:44:11 +0500

    Adding path of json file, sendPush() method
    In python forward slashes are used in file address.

--- FCMManager.py
+++ FCMManager.py
@@ -1,5 +1,21 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+#  Remember the forward Slashes
+
+cred = credentials.Certificate(
+    "G:/python_workspaces/pushNotifications/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+
+def sendPush(title, body, token):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        token=token,
+    )
+
+    response = messaging.send(message)
+    print('Successfully sent message:', response)
